6a0c8f466bfa419a041c38e3abed3814c9bbc00b
fayekafateh <111707631+fayekafateh@users.noreply.github.com>
2023-01-12 21:49:39 -0500

    19.2

--- 19.2/01_Conditionals/unsolved/js/index.js
+++ 19.2/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,19 @@
 $('.tab').on("click", function(){
+    if($(this).hasClass("tab1")) {
+        $(".tab1").addClass("active");
+        $("#tabContent1").css("display","block");
+        $(".tab2").removeClass("active");
+        $("#tabContent2").css("display","none");
+        
+
+    }
+    else if($(this).hasClass("tab2")) {
+        $(".tab1").removeClass("active");
+        $("#tabContent1").css("display","none");
+        $(".tab2").addClass("active");
+        $("#tabContent2").css("display","block");
+
+    }
+
   
 });
